55c32cee714fec1db1a64d4be723c09405576d22
JAROD MICHAEL ARMANDO CHUB BOLANOS <jmchubb@correo.url.edu.gt>
2023-03-23 09:57:28 -0600

    RESOLVER EJERCICIO03

--- Laboratorio04/Ejercicio03.cs
+++ Laboratorio04/Ejercicio03.cs
@@ -8,9 +8,16 @@ namespace Laboratorio04
 {
     public class Ejercicio03
     {
-        public string Sumar(string num1, string num2) 
+        public string Sumar(string num1, string num2)
         {
-            return "";
+            decimal suma = 0;
+            if (Convert.ToDecimal(num1) >= 0 && Convert.ToDecimal(num2) >= 0)
+            
+                {
+                    suma = Convert.ToDecimal(num1) + Convert.ToDecimal(num2);
+                }
+                return Convert.ToString(suma);
+            
         }
     }
-}
+} 
