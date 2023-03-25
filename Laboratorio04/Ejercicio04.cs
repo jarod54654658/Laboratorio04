fe6bc9f7d89e06265d39ff35d964230218663725
JAROD MICHAEL ARMANDO CHUB BOLANOS <jmchubb@correo.url.edu.gt>
2023-03-25 08:54:10 -0600

    lab4 prueba

--- Laboratorio04/Ejercicio04.cs
+++ Laboratorio04/Ejercicio04.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices; 
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,53 @@ namespace Laboratorio04
 {
     public class Ejercicio04
     {
-        public int[] CalcularBilletes(int cantidad, int[] billetes)
+        public int[] CalcularBilletes(int cantidad[0, 0, 0, 0, 0],  int billetes[100, 50, 20, 5, 1])
         {
-            return new int[] {};
+
+            Console.WriteLine("Ingrese cantidad");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            if (cantidad>=100)
+            {
+                int div = cantidad % 100;
+                int almacenado = cantidad - div;
+                cantidad = cantidad - almacenado;
+
+
+            }
+            if (cantidad >= 50)
+            {
+                int div = cantidad % 50;
+                int almacenado = cantidad - div;
+                cantidad = cantidad - almacenado;
+
+
+            }
+            if (cantidad >= 20)
+            {
+                int div = cantidad % 20;
+                int almacenado = cantidad - div;
+                cantidad = cantidad - almacenado;
+
+
+            }
+            if (cantidad >= 5)
+            {
+                int div = cantidad % 5;
+                int almacenado = cantidad - div;
+                cantidad = cantidad - almacenado;
+
+
+            }
+            if (cantidad >= 1)
+            {
+                int div = cantidad % 1;
+                int almacenado = cantidad - div;
+                cantidad = cantidad - almacenado;
+
+
+            }
+           
+            return new int [0, 0, 0, 0, 0, 0] {};
         }
     }
 }
