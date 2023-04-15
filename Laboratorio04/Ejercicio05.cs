8a420d8ae67aae83b28c236a998e23b1deee7955
JAROD MICHAEL ARMANDO CHUB BOLANOS <jmchubb@correo.url.edu.gt>
2023-04-14 19:13:53 -0600

    ejercicio05

--- Laboratorio04/Ejercicio05.cs
+++ Laboratorio04/Ejercicio05.cs
@@ -8,9 +8,33 @@ namespace Laboratorio04
 {
     public class Ejercicio05
     {
-        public int[] CalcularBilletes(int cantidad, int[,] billetes)
+        public int[] CalcularBilletes(int monto, int[,] billetes)
         {
-            return new int[] {};
+            int[] total = new int[billetes.GetLength(0)];
+
+            for (int i = 0; i < total.Length; i++)
+            {
+                int billetesdeno = billetes[i, 0];
+                int numerobillete = billetes[i, 1];
+
+                int billetealeatorio = monto / billetesdeno;
+
+                if (numerobillete < 0 || billetealeatorio <= numerobillete)
+                {
+                    total[i] = billetealeatorio;
+                }
+                else
+                {
+                    total[i] = numerobillete;
+                }
+
+                monto =   monto - (total[i] * billetesdeno);
+            }
+
+
+            Console.ReadKey();
+            return total;
+            
         }
     }
 }
